51b86b93bfef977af21cfea219987fab9c835e22
7abhishek7 <74947815+7abhishek7@users.noreply.github.com>
2020-12-01 16:06:19 +0530

    exercise

---
+++ Abhishek Chandel_843972/Afternoon/Spring AOP/src/main/java/com/org/Main.java
@@ -0,0 +1,20 @@
+package com.org;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		EmployeeService service = (EmployeeService) context.getBean("employeeService");
+		service.delete();
+		service.store();
+		service.getEmployee(2);
+		service.getAllEmployee(4,"abhish");
+		service.loadedEmployee(5, 1000);
+		service.updateEmployee(8);
+	}
+
+}
